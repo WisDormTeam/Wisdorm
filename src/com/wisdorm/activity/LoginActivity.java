ff8d8cf1addb7316ce1cf0aa403f677a8de33f49
fxy1234555 <353896263@qq.com>
2014-10-20 14:35:51 +0800

    by fxy

--- src/com/wisdorm/activity/LoginActivity.java
+++ src/com/wisdorm/activity/LoginActivity.java
@@ -2,10 +2,12 @@ package com.wisdorm.activity;
 /*
  * test here~~~~/
  */
+
 import com.example.wisdorm.R;
 import com.example.wisdorm.R.id;
 import com.example.wisdorm.R.layout;
 import com.wisdorm.base.MytListener;
+import com.wisdorm.bmob.DebugTool;
 import com.wisdorm.common.LoginListener;
 import com.wisdorm.common.Message.LoginMessage;
 import com.wisdorm.common.Message.RegisterMessage;
@@ -17,8 +19,13 @@ import cn.bmob.v3.Bmob;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -26,78 +33,91 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.os.Build;
 
 public class LoginActivity extends Activity {
+	
+	private static final String TAG_POST = null;
+	public EditText username, password;
+	public Button btn_login;
+	public CheckBox checkPwd;
+	private String name;
+	private String pass;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
+		username = (EditText)findViewById(R.id.username);
+		password = (EditText)findViewById(R.id.password);
+		btn_login = (Button)findViewById(R.id.login);
+		
+		btn_login.setOnClickListener(new MyOnClickListener());
 		
 		//init Bmob here
 		initBmob();
 		
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-		
-		
 		ActivityManager.getInstance().setLoginActivity(this);
 		
 		//test
-		Intent intent = new Intent(LoginActivity.this, com.wisdorm.ui.MainActivity.class);
-		startActivity(intent);
+		//Intent intent = new Intent(LoginActivity.this, com.wisdorm.ui.MainActivity.class);
+		//startActivity(intent);
 	}
 	
 	private void initBmob(){
 		Bmob.initialize(this, "81ea98fa1507d9f34639cc0b138770fb");
 	}
-
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
+	
+	public class MyOnClickListener implements View.OnClickListener{
 
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_login,
-					container, false);
-			return rootView;
-		}
-		
-		@Override
-		public void onActivityCreated(Bundle savedInstanceState) {
+		public void onClick(View arg0) {
 			// TODO Auto-generated method stub
-			super.onActivityCreated(savedInstanceState);
-			Button login = (Button) getActivity().findViewById(R.id.login);
-			login.setOnClickListener(new View.OnClickListener() {
+			name = username.getText().toString();
+			pass = password.getText().toString();
+			if(TextUtils.isEmpty(name) || TextUtils.isEmpty(pass))
+			{
+				Toast.makeText(LoginActivity.this, "用户名或者密码不能为空",
+						Toast.LENGTH_LONG).show();
+			}
+		
+			NetworkManager nm = AppController.getInstance().getNetworkManager();
+			nm.send(new LoginMessage(name, pass), new MytListener() {
 				
 				@Override
-				public void onClick(View arg0) {
-					Log.d("bmob", "Test Register");
-					System.out.println("suasaasasa");
-					NetworkManager nm = AppController.getInstance().getNetworkManager();
-					nm.send(new RegisterMessage("407471516@qq.com","123456"),new MytListener() {
-						
-						@Override
-						public void onSuccess() {
-							// TODO Auto-generated method stub
-							Log.d("bmob", "Register succeed");
-						}
-						
-						@Override
-						public void onFailure(String failMsg) {
-							// TODO Auto-generated method stub
-							Log.d("bmob", failMsg);
-						}
-					});
+				public void onSuccess() {
+					// TODO Auto-generated method stub
+					Log.v("debug", "success");
+					
+					if(AppController.getInstance().getUserManager().getUser().getDormId() == null)
+					{
+						DebugTool.getInstance().log("null!!!!!!");
+						////Intent intent = new Intent(LoginActivity.this, com.wisdorm.ui.MainActivity.class);
+						//startActivity(intent);
+					}
+					else {
+						DebugTool.getInstance().log("dsdsds");
+					}
+				}
+				
+				@Override
+				public void onFailure(String failMsg) {
+					// TODO Auto-generated method stub
+					Log.v("debug", "failure");
+					Toast.makeText(LoginActivity.this, "登陆失败"+failMsg,
+							Toast.LENGTH_LONG).show();
 				}
 			});
+			
 		}
+
 	}
+
+	
 
 }
