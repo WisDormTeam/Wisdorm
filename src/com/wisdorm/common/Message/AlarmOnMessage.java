4acd2b4ce7d2f90491c2751d84bf440790a50bc2
CBUU <ioricbuu@gmail.com>
2014-10-25 00:55:14 +0800

    debug

--- src/com/wisdorm/common/Message/AlarmOnMessage.java
+++ src/com/wisdorm/common/Message/AlarmOnMessage.java
@@ -5,6 +5,6 @@ import com.wisdorm.base.MessageBase;
 public class AlarmOnMessage extends MessageBase{
 	public AlarmOnMessage() {
 		// TODO Auto-generated constructor stub
-		this.msgType = MSG_ALARM_OFF;
+		this.msgType = MSG_AlARM_ON;
 	}
 }
